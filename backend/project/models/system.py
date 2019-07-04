c02c7cd4a0e2a1fac5dfc6c15feeccd760330582
Anna Ung <z5115212@student.unsw.edu.au>
2019-07-05 00:55:03 +1000

    added system class? delete if not needed

---
+++ backend/project/models/system.py
@@ -0,0 +1,15 @@
+# backend/project/models/user.py
+
+class System():
+	def __init__ (self):
+		self._allListings = []
+
+	@property
+	def allListings(self):
+		return self._allListings
+
+
+# class to put all listings together?
+
+	
+
