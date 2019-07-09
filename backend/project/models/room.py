5870cdd0e11e4c78837c529010d9ccf728a2223d
Anna Ung <z5115212@student.unsw.edu.au>
2019-07-09 11:29:24 +1000

    merged room with auth

--- backend/project/models/room.py
+++ backend/project/models/room.py
@@ -17,8 +17,49 @@ class Room(db.Model):
     listing_id = db.Column(db.Integer, db.ForeignKey('Listing.id'), nullable=False)
 
     def __init__(self, roomType, cost, furnished, available_from, min_stay):
-        self.roomType = roomType
-        self.cost = cost
-        self.furnished = furnished
-        self.availability = datetime.strptime(available_from, "%d-%m-%Y")
-        self.min_stay = min_stay
+        self._roomType = roomType
+        self._cost = cost
+        self._furnished = furnished
+        self._availability = datetime.strptime(available_from, "%d-%m-%Y")
+        self._min_stay = min_stay
+
+	@property
+	def roomType(self):
+		return self._roomType
+
+	@roomType.setter
+	def roomType(self, var):
+		self._roomType = var
+		
+	@property
+	def cost(self):
+		return self._cost
+
+	@cost.setter
+	def cost(self, var):
+		self._cost = var
+
+	@property
+	def furnished(self):
+		return self._furnished
+
+	@furnished.setter
+	def furnished(self, var):
+		self._furnished = var
+
+	@property
+	def available_from(self):
+		return self._availability
+
+	@available_from.setter
+	def available_from(self, y, m, d):
+		self._availability = datetime.strptime(available_from, "%d-%m-%Y")
+
+	@property
+	def min_stay(self):
+		return self._min_stay
+		
+	@min_stay.setter
+	def min_stay(self, var):
+		self._min_stay = var
+		
