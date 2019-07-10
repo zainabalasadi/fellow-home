8a4a72fefc7635eb90fef23f88038e3e0b35a315
Anna Ung <z5115212@student.unsw.edu.au>
2019-07-10 18:05:31 +1000

    commented out lists

--- backend/project/models/listing.py
+++ backend/project/models/listing.py
@@ -22,9 +22,9 @@ class Listing(db.Model):
     def __init__(self, name, date_published, num_housemates, num_vacancies):
         self._name = name
         self._date_published = date_published
-        self.address = address
+        # self.address = address
         # self.images = []
-        #self.feature = feature
+        # self.feature = feature
         self._num_housemates = num_housemates
         self._num_vacancies = num_vacancies
         # self.restrictions = []
@@ -46,13 +46,13 @@ class Listing(db.Model):
     def date_published(self, var):
         self._date_published = var
 
-    @property
-    def images(self):
-        return self.images
+    # @property
+    # def images(self):
+    #     return self.images
     
-    @images.setter
-    def images(self, var):
-        self._images = var
+    # @images.setter
+    # def images(self, var):
+    #     self._images = var
 
     @property
     def num_housemates(self):
@@ -70,38 +70,38 @@ class Listing(db.Model):
     def num_vacancies(self, var):
         self._num_vacancies = var
 
-    @property
-    def restrictions(self):
-        return self._restrictions
+    # @property
+    # def restrictions(self):
+    #     return self._restrictions
 
-    @restrictions.setter
-    def restrictions(self, var):
-        self._restrictions = var
+    # @restrictions.setter
+    # def restrictions(self, var):
+    #     self._restrictions = var
 
-    @property
-    def room(self):
-        return self._room
+    # @property
+    # def room(self):
+    #     return self._room
 
-    @room.setter
-    def setter(self, var):
-        self._room = var
+    # @room.setter
+    # def setter(self, var):
+        # self._room = var
 
-    def addImage(self, var):
-        self._image.append(var)
+    # def addImage(self, var):
+    #     self._image.append(var)
 
-    def deleteImage(self, var):
-        self._image.remove(var)
+    # def deleteImage(self, var):
+    #     self._image.remove(var)
     
-    def addRoom(self, var):
-        self._room.append(var)
+    # def addRoom(self, var):
+    #     self._room.append(var)
 
-    def deleteRoom(self, var):
-        self._room.remove(var)
+    # def deleteRoom(self, var):
+    #     self._room.remove(var)
 
-    def addRestrictions(self, var):
-        self._restrictions.append(var)
+    # def addRestrictions(self, var):
+    #     self._restrictions.append(var)
 
-    def deleteRestrictions(self, var):
-        self._restrictions.remove(var)
+    # def deleteRestrictions(self, var):
+    #     self._restrictions.remove(var)
     
     
