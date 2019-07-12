4f6ed85cd4899d1fb77136f5eb0844779af4f525
Anna Ung <z5115212@student.unsw.edu.au>
2019-07-12 13:23:42 +1000

    added exception handling for listing

--- backend/project/controllers/routes.py
+++ backend/project/controllers/routes.py
@@ -5,6 +5,8 @@ from flask_praetorian import auth_required
 
 from project import guard
 from project.models.user import User
+from project.models.listing import Listing
+from project.models.error import SignUpError, DetailError
 from project.models.blacklist import TokenBlacklist
 
 bp = Blueprint('routes', __name__)
@@ -91,15 +93,40 @@ def logout():
 @bp.route('/editprofile',methods=['GET','POST'])
 def editprofile():
     if request.method == "POST":
-        contact = request.form["contact"]
-        about = request.form["about"]
-        school = request.form["school"]
-        study = request.form["study"]
+        req = request.get_json()
+        contact = req["contact"]
+        about = req["about"]
+        school = req["school"]
+        study = req["study"]
         return 'editing profile'
 
 @bp.route('/postproperty',methods=['GET','POST'])
 def postproperty():
-    return
+    def editprofile():
+    if request.method == "POST":
+        req = request.get_json()
+        name = req['name']
+        date_published = req['date_published']
+        num_housemates = req['num_housemates']
+        num_vacancies = req['num_vacancies']
+        num_bedrooms = req['num_bedrooms']
+        has_garden = req['has_garden']
+        landsize = req['landsize']
+        try:
+            if (name == ""):
+                raise DetailError("Please provide a title")
+            elif (num_housemates < 0):
+                raise DetailError("Please provide the number of housemates")
+            elif (num_vacancies < 0):
+                raise DetailError("Please provide the number of vacancies")
+            elif (num_bedrooms < 0):
+                raise DetailError("Please provide the number of bedrooms")
+            elif (landsize < 0):
+                raise DetailError("Please provide the landsize")
+        except DetailError as error:
+            return error.message
+        Listing.add(name=name, date_published=date_published, num_housemates=num_housemates, num_vacancies=num_vacancies, num_bedrooms=num_bedrooms, has_garden=has_garden, landsize=landsize)
+        return jsonify(status='success', msg='successfully created listing'), 200
 
 @bp.route('/editproperty',methods=['GET','POST'])
 def editproperty():
--- backend/project/models/error.py
+++ backend/project/models/error.py
@@ -1,3 +1,5 @@
+# backend/project/models/error.py
+
 class SignUpError(Exception):
     def __init__(self, message):
         super().__init__()
@@ -5,3 +7,11 @@ class SignUpError(Exception):
 
     def __str__(self):
         return "{}".format(self.message)
+
+class DetailError(Exception):
+    def __init__(self, message):
+        super().__init__()
+        self._message = message
+
+    def __str__(self):
+        return "{}".format(self._message)
