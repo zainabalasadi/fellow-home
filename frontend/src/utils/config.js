ef4ee9d6e2d4af3924fcae6180aa82b539c2274f
Kibanos <37525207+Kibanos@users.noreply.github.com>
2019-07-16 11:53:03 +1000

    config file with features
    for ease of editing menus and checkboxes etc.

---
+++ frontend/src/utils/config.js
@@ -0,0 +1,16 @@
+
+const  checkFeatures = [
+    'Attic',
+    'Basement',
+    'Kitchen',
+    'Garden'
+];
+const dropFeatures= [
+    { value: "7", label: "7"},
+    { value: "8", label: "8"},
+    { value: "9", label: "9"}
+]
+module.exports = {
+    checkFeatures: checkFeatures,
+    dropFeatures: dropFeatures
+}
